25957c72e8128d3d68d6e4faff651f43a2bf28a7
Hannah Schuster <hschuster9@gmail.com>
2017-01-31 14:53:55 -0500

    number of guesses

--- script.js
+++ script.js
@@ -1,90 +1,85 @@
 
-alert("Player 1: Type a word for Player 2 to guess where it says, 'Enter word' then click 'Submit'");
-
+//alert("Player 1: Type a word for Player 2 to guess where it says, 'Enter word' then click 'Submit'");
+$(".enter-letters").hide();
 var checkedArray = [];
 var incorrectLetters = [];
 var wordArray = [];
+
 $("#set-word").on("click", enterWord);
+  function enterWord () {
 
-function enterWord () {
+    event.preventDefault();
+    $(".enter-words").toggle();
+    $(".enter-letters").show();
+    //store word entered into variable
+    var wordInput = $("#word").val();
+    console.log(wordInput);
 
-  event.preventDefault();
-  $('.enter-words').toggle();
-  //store word entered into variable
-  var wordInput = $("#word").val();
-  console.log(wordInput);
-  //split the letters in word
-  //put letters into array
-  var wordInputSplitArray = wordInput.split("");
-  console.log(wordInputSplitArray);
-  //replace letters with dashes
-  var wordArray = [];
-  for( var i = 0; i < wordInputSplitArray.length; i++){
-    checkedArray.push("-");
-    wordArray.push("-");
-}
-
-  alert("Player 2: Guess the letters of the word by typing one letter at a time where it says 'Enter letter'");
-
+    //split the letters in word and put letters into array
+    var wordInputSplitArray = wordInput.split("");
+    console.log(wordInputSplitArray);
+    //replace letters with dashes
+    var wordArray = [];
+    for( var i = 0; i < wordInputSplitArray.length; i++){
+      checkedArray.push("-");
+      wordArray.push("-");
+        }
   //display dashes on page
   $("#letterdashes").text(wordArray.join(' '));
-
+  //alert for player 2's turn
+  alert("Player 2: Guess the letters of the word where it says 'Enter letter'");
 };
 
-//alert("Player 2: Guess the letters of the word by typing one letter at a time where it says 'Enter letter'");
-//when you click "submit" for enter letter input run displayLetters function
+//when you click "submit" for enter letter input
 $("#guess-letter").on("click", displayLetters);
-
 function displayLetters(){
-  event.preventDefault();
-  var wordInput = $("#word").val();
-  var wordInputSplitArray = wordInput.split("");
-  var guessedLetter = $("#letter").val();
-  var image = [];
-      image[1] = '<img src="hangman2.jpeg">';
-      image[2] = '<img src="hangman3.jpeg">';
-      image[3] = '<img src="hangman4.jpeg">';
-      image[4] = '<img src="hangman5.jpeg">';
-      image[5] = '<img src="hangman6.jpeg">';
-      image[6] = '<img src="hangman7.jpeg">';
-  console.log(guessedLetter);
 
-var isMatched = false;
-  for(var i = 0; i < wordInputSplitArray.length; i++){
-    if(wordInputSplitArray[i] == guessedLetter) {
-      checkedArray[i] = guessedLetter;
-      console.log(checkedArray);
-      console.log(wordArray);
-      isMatched = true;
-      console.log(`it's a match at index: $[i]`);
-    }
+    event.preventDefault();
+    var wordInput = $("#word").val();
+    var wordInputSplitArray = wordInput.split("");
+    var guessedLetter = $("#letter").val();
+    var image = [];
+        image[1] = '<img src="hangman2.jpeg">';
+        image[2] = '<img src="hangman3.jpeg">';
+        image[3] = '<img src="hangman4.jpeg">';
+        image[4] = '<img src="hangman5.jpeg">';
+        image[5] = '<img src="hangman6.jpeg">';
+        image[6] = '<img src="hangman7.jpeg">';
+    console.log(guessedLetter);
 
-  }
-  if (isMatched == false){
+    var isMatched = false;
+      for(var i = 0; i < wordInputSplitArray.length; i++){
+        if(wordInputSplitArray[i] == guessedLetter) {
+          checkedArray[i] = guessedLetter;
+          isMatched = true;
+          console.log("Match at index: " + [i]);
+        }
 
-    incorrectLetters.push(guessedLetter);
-    console.log(incorrectLetters);
+      }
+        if (isMatched == false){
 
-    var numberOfChances = image.length - 1;
-    var numberofChancesLeft = numberOfChances--;
-    console.log(numberofChancesLeft);
-    console.log(numberOfChances);
-  if (wrongGuesses > numberOfChances) {
-    alert("game over");
-  }
+          incorrectLetters.push(guessedLetter);
+          console.log(incorrectLetters);
 
-    //image is an array of all images
-    //define var for number of wrong guesses, aka incorrectLetters.length
-    var wrongGuesses = incorrectLetters.length;
-    $('div .hangman').html(image[wrongGuesses]);
+          //image is an array of all images
+          var numberOfChances = image.length -2;
+          console.log(numberOfChances);
 
-  }
 
-  var output = checkedArray.join("");
-  console.log(output);
-  console.log(incorrectLetters);
+          //define var for number of wrong guesses, aka incorrectLetters.length
+          var wrongGuesses = incorrectLetters.length;
+          if (wrongGuesses > numberOfChances) {
+          alert("Game over");
+        }
 
-  $("#letterdashes").text(output);
-  $("#incorrect").text(incorrectLetters);
-}
-;
+          $('div .hangman').html(image[wrongGuesses]);
+
+        }
+
+        var output = checkedArray.join("");
+        console.log(output);
+        console.log(incorrectLetters);
+
+        $("#letterdashes").text(output);
+        $("#incorrect").text(incorrectLetters);
+      };
