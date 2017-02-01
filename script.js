e3af5d2c84c48add9b1ce5820b5423cd2d01e97a
Hannah Schuster <hschuster9@gmail.com>
2017-01-31 19:04:30 -0500

    updated

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $("#set-word").on("click", enterWord);
   function enterWord () {
 
     event.preventDefault();
-    $(".enter-words").toggle();
+    $(".enter-words").hide();
     $(".enter-letters").show();
     //store word entered into variable
     var wordInput = $("#word").val();
@@ -27,7 +27,7 @@ $("#set-word").on("click", enterWord);
   //display dashes on page
   $("#letterdashes").text(wordArray.join(' '));
   //alert for player 2's turn
-  alert("Player 2: Guess the letters of the word where it says 'Enter letter'");
+  alert("Player 2: Guess the letters of the word where it says, 'Enter letter'");
 };
 
 //when you click "submit" for enter letter input
@@ -39,15 +39,18 @@ function displayLetters(){
     var wordInputSplitArray = wordInput.split("");
     var guessedLetter = $("#letter").val();
     var image = [];
-        image[1] = '<img src="hangman2.jpeg">';
-        image[2] = '<img src="hangman3.jpeg">';
-        image[3] = '<img src="hangman4.jpeg">';
-        image[4] = '<img src="hangman5.jpeg">';
-        image[5] = '<img src="hangman6.jpeg">';
-        image[6] = '<img src="hangman7.jpeg">';
+        image[1] = '<img src="images/hangman2.jpeg">';
+        image[2] = '<img src="images/hangman3.jpeg">';
+        image[3] = '<img src="images/hangman4.jpeg">';
+        image[4] = '<img src="images/hangman5.jpeg">';
+        image[5] = '<img src="images/hangman6.jpeg">';
+        image[6] = '<img src="images/hangman7.jpeg">';
     console.log(guessedLetter);
 
+    //var to check to see if guess matches letter -> false for wrong
     var isMatched = false;
+
+    //loop through word to find match
       for(var i = 0; i < wordInputSplitArray.length; i++){
         if(wordInputSplitArray[i] == guessedLetter) {
           checkedArray[i] = guessedLetter;
@@ -56,30 +59,27 @@ function displayLetters(){
         }
 
       }
+        //incorrect guess
         if (isMatched == false){
 
-          incorrectLetters.push(guessedLetter);
+          //pushes guessed letter to display in incorrect letters
+          incorrectLetters.push(" " + guessedLetter);
           console.log(incorrectLetters);
 
-          //image is an array of all images
-          var numberOfChances = image.length -2;
+          //all images in array
+          var numberOfChances = image.length - 2;
           console.log(numberOfChances);
 
-
-          //define var for number of wrong guesses, aka incorrectLetters.length
+          //define var for number of wrong guesses --> incorrectLetters.length
           var wrongGuesses = incorrectLetters.length;
           if (wrongGuesses > numberOfChances) {
           alert("Game over");
         }
-
           $('div .hangman').html(image[wrongGuesses]);
 
         }
 
         var output = checkedArray.join("");
-        console.log(output);
-        console.log(incorrectLetters);
-
         $("#letterdashes").text(output);
         $("#incorrect").text(incorrectLetters);
       };
